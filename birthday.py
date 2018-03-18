7672b2a5247072b708ad2f395a038d5ba3bcc312
Donghyun Kim <digitalisx99@gmail.com>
2018-03-18 23:55:09 +0900

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -2,7 +2,7 @@ import os
 import datetime
 
 birthday = datetime.date.today()
-birthday_convert = str(brithday.isoformat())
+birthday_convert = str(birthday.isoformat())
 
 if(birthday_convert == "1999-01-08"):
   print("Congratulation, You are a good python student! :)")
